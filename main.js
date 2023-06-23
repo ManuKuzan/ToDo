bebbf37cf9425a5afb290f18c67b50b1cfe35ec1
ManuKuzan <136617152+ManuKuzan@users.noreply.github.com>
2023-06-22 21:00:18 -0600

    Arreglo
    finalizado

--- main.js
+++ main.js
@@ -2,36 +2,33 @@ const input = document.querySelector("input");
 const addBtn = document.querySelector(".btn-add");
 const ul = document.querySelector("#elementList");
 const empty = document.querySelector(".empty");
-const contaTarea=document.querySelector("#contaTarea");
-addBtn.addEventListener('keypress',addTask);
+const contaTarea = document.querySelector("#contaTarea");
+addBtn.addEventListener("keypress", addTask);
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-addTask();
-
+  addTask();
 });
-function addTask(){
-  const text = input.value;
+function addTask() {
+  const text = input.value.toLowerCase(); // Convertir a minúsculas
 
-  var lista_tarea =document.querySelectorAll(".textoTarea");
-  var ExisteTarea =false;
-  for(let index=0;index<lista_tarea.length;index++){
-    var TextoP= lista_tarea[index];
-    if(TextoP.textContent==text){
-      ExisteTarea=true;
+  var lista_tarea = document.querySelectorAll(".textoTarea");
+  var ExisteTarea = false;
 
+  for (let index = 0; index < lista_tarea.length; index++) {
+    var TextoP = lista_tarea[index];
+    if (TextoP.textContent.toLowerCase() === text) {
+      // Comparar en minúsculas
+      ExisteTarea = true;
+      break; // Salir del bucle si se encuentra una tarea repetida
     }
   }
 
-  if (input.value == 0) {
-    alert("Agrega una tarea")
-
-
-  }else if(ExisteTarea==true){
-    alert("Ya Existe esa tarea, agrega otra")
-
-return;
-  } else{
+  if (text.length === 0) {
+    alert("Agrega una tarea");
+  } else if (ExisteTarea) {
+    alert("Ya existe esa tarea, agrega otra");
+  } else {
     var check = document.createElement("input");
     check.type = "checkbox";
     check.classList.add("check-button");
@@ -47,28 +44,22 @@ return;
     li.appendChild(addDeleteBtn());
     ul.appendChild(li);
 
-
     input.value = "";
     empty.style.display = "none";
-
   }
-
 }
-function count(){
+function count() {
   var contador = 0;
   var lista = document.querySelectorAll("input[type='checkbox']");
 
   for (let index = 0; index < lista.length; index++) {
     if (lista[index].checked) {
       contador++;
-     
     }
-    
   }
-  contaTarea.textContent=contador;
- return  console.log(contador);
-
-};
+  contaTarea.textContent = contador;
+  return console.log(contador);
+}
 function addDeleteBtn() {
   const deleteBtn = document.createElement("button");
 
@@ -82,16 +73,10 @@ function addDeleteBtn() {
     const items = document.querySelectorAll("li");
 
     if (items.length === 0) {
-
       empty.style.display = "block";
-
-
-    }count();
+    }
+    count();
   });
 
   return deleteBtn;
 }
-
-
-
-
